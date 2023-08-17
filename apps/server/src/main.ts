0094c2807b79a6dc8a695aa02b0e31df8c5046dd
JoropoKing <gustavo.serrano.dev@proton.me>
2023-08-17 10:17:47 -0400

    (fix): added class validator

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigType } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -27,6 +27,8 @@ async function bootstrap() {
 
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   const port = config.app.port!;
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(port);
   Logger.log(
--- libs/dto/src/lib/create-item.dto.ts
+++ libs/dto/src/lib/create-item.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class CreateItemDto {
+  @IsString()
   @ApiProperty()
   name: string;
 
+  @IsNumber()
   @ApiProperty()
   price: number;
 
+  @IsString()
   @ApiProperty()
   description: string;
 }
