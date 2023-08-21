4ad03f59c8fc42794dc59c32616f851a49adbf75
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-08-21 19:50:27 -0300

    almost finished

--- apps/server/src/dbseed/dbseed.controller.ts
+++ apps/server/src/dbseed/dbseed.controller.ts
@@ -5,14 +5,58 @@ import { DbseedService } from './dbseed.service';
 export class DbseedController {
   constructor(private readonly dbseedService: DbseedService) {}
 
-  @Post('save')
+  @Post('saveMovies')
   movieGenres() {
     return this.dbseedService.addMovieGenres();
   }
 
-  @Get('list')
+  @Get('listMovies')
   async getMovieGenres() {
     const belongsTo = 'Películas';
+    return this.dbseedService.findByBelongsTo(belongsTo);
+  }
+
+  @Post('saveSeries')
+  serieGenres() {
+    return this.dbseedService.addSerieGenres();
+  }
+
+  @Get('listSeries')
+  async getSerieGenres() {
+    const belongsTo = 'Series';
+    return this.dbseedService.findByBelongsTo(belongsTo);
+  }
+
+  @Post('saveAnimes')
+  animeGenres() {
+    return this.dbseedService.addAnimeGenres();
+  }
+
+  @Get('listAnimes')
+  async getAnimeGenres() {
+    const belongsTo = 'Animes';
+    return this.dbseedService.findByBelongsTo(belongsTo);
+  }
+
+  @Post('saveVideogames')
+  videogameGenres() {
+    return this.dbseedService.addVideogameGenres();
+  }
+
+  @Get('listVideogames')
+  async getVideogameGenres() {
+    const belongsTo = 'Videojuegos';
+    return this.dbseedService.findByBelongsTo(belongsTo);
+  }
+
+  @Post('saveMusic')
+  musicGenres() {
+    return this.dbseedService.addMusicGenres();
+  }
+
+  @Get('listMusic')
+  async getMusicGenres() {
+    const belongsTo = 'Música';
     return this.dbseedService.findByBelongsTo(belongsTo);
   }
 }
--- apps/server/src/dbseed/dbseed.service.ts
+++ apps/server/src/dbseed/dbseed.service.ts
@@ -17,7 +17,7 @@ export class DbseedService {
     const { data } = await firstValueFrom(
       this.httpService
         .get<{ genres: { name: string }[] }>(
-          `${api}?language=es&api_key=${apiKey}`
+          `${api}?language=en&api_key=${apiKey}`
         )
         .pipe(
           catchError((error) => {
@@ -28,6 +28,103 @@ export class DbseedService {
     );
     const transformedData = data.genres.map((genre) => genre.name);
     const genres = { belongsTo: 'Películas', genres: transformedData };
+    try {
+      await this.genresService.saveGenres(genres);
+      console.log('Genres saved successfully');
+    } catch (error) {
+      console.log('Error saving genres', error);
+    }
+    return genres;
+  }
+
+  async addSerieGenres(): Promise<Genres> {
+    const api = 'https://api.themoviedb.org/3/genre/tv/list';
+    const apiKey = 'a0e1f02b394263b862d094dbc96d422c';
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<{ genres: { name: string }[] }>(
+          `${api}?language=es&api_key=${apiKey}`
+        )
+        .pipe(
+          catchError((error) => {
+            console.log(error.response.data);
+            throw 'An error happened retrieving series genres!';
+          })
+        )
+    );
+    const transformedData = data.genres.map((genre) => genre.name);
+    const genres = { belongsTo: 'Series', genres: transformedData };
+    try {
+      await this.genresService.saveGenres(genres);
+      console.log('Genres saved successfully');
+    } catch (error) {
+      console.log('Error saving genres', error);
+    }
+    return genres;
+  }
+
+  async addAnimeGenres(): Promise<Genres> {
+    const api = 'https://api.jikan.moe/v4/genres/anime';
+    const { data } = await firstValueFrom(
+      this.httpService.get<{ data: { name: string }[] }>(`${api}`).pipe(
+        catchError((error) => {
+          console.log(error.response.data);
+          throw 'An error happened retrieving animes genres!';
+        })
+      )
+    );
+    const transformedData = data.data.map((genre) => genre.name);
+    const genres = { belongsTo: 'Animes', genres: transformedData };
+    try {
+      await this.genresService.saveGenres(genres);
+      console.log('Genres saved successfully');
+    } catch (error) {
+      console.log('Error saving genres', error);
+    }
+    return genres;
+  }
+
+  async addVideogameGenres(): Promise<Genres> {
+    const api = 'https://api.rawg.io/api/genres';
+    const apiKey = '5a56a1e8a66e4d69859ab6849a755615';
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<{ results: { name: string }[] }>(`${api}?key=${apiKey}`)
+        .pipe(
+          catchError((error) => {
+            console.log(error.response.data);
+            throw 'An error happened retrieving videogame genres!';
+          })
+        )
+    );
+    const transformedData = data.results.map((genre) => genre.name);
+    const genres = { belongsTo: 'Videojuegos', genres: transformedData };
+    try {
+      await this.genresService.saveGenres(genres);
+      console.log('Genres saved successfully');
+    } catch (error) {
+      console.log('Error saving genres', error);
+    }
+    return genres;
+  }
+
+  async addMusicGenres(): Promise<Genres> {
+    const api = 'https://api.themoviedb.org/3/genre/movie/list';
+    const apiKey = 'a0e1f02b394263b862d094dbc96d422c';
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<{ genres: { name: string }[] }>(
+          `${api}?language=es&api_key=${apiKey}`
+        )
+        .pipe(
+          catchError((error) => {
+            console.log(error.response.data);
+            throw 'An error happened retrieving music genres!';
+          })
+        )
+    );
+    const transformedData = data.genres.map((genre) => genre.name);
+    const genres = { belongsTo: 'Música', genres: transformedData };
     try {
       await this.genresService.saveGenres(genres);
       console.log('Genres saved successfully');
