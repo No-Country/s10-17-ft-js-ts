ffec32c400784bd0da9f7bd134f4deae89a35959
Jefersonsteven <gfreycode@gmail.com>
2023-08-22 09:35:51 -0500

    (feat): create header component

---
+++ apps/client/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+'use client'
+import style from './style.module.scss'
+
+export default function Header () {
+  return (
+    <header className={style.header}>
+      <h1 className={style.title}>Header</h1>
+    </header>
+  )
+}
