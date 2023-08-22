6e229b18f6a16ccad52ade43ea2972aa5652be82
Jefersonsteven <gfreycode@gmail.com>
2023-08-22 15:58:03 -0500

    (feat): create header v1

--- apps/client/components/Header/Header.tsx
+++ apps/client/components/Header/Header.tsx
@@ -1,10 +1,58 @@
 'use client'
+import Link from 'next/link'
 import style from './style.module.scss'
 
 export default function Header () {
   return (
     <header className={style.header}>
-      <h1 className={style.title}>Header</h1>
+        <span>Logo</span>
+        <nav className={style.header__navbar}>
+          <ul className={style.header__menu}>
+            <div className={style.header__navigation}>
+              <li className='pointer'>
+                <Link href={'#'}>Matches</Link>
+              </li>
+              <li className='pointer'>
+                <Link href={'#'}>Mensajes</Link>
+              </li>
+              <li className="pointer">
+                <details>
+                  <summary>Informacion</summary>
+                  <ul className={style.header__submenu}>
+                    <li className='pointer'>
+                      <Link href={'#'}>Quienes somos</Link>
+                    </li>
+                    <li className='pointer'>
+                      <Link href={'#'}>Avisos Legales</Link>
+                    </li>
+                    <li className='pointer'>
+                      <Link href={'#'}>Contacto</Link>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+            </div>
+            <li className='pointer'>
+            <details>
+                <summary>Perfil</summary>
+                <ul className={style.header__submenu}>
+                  <li className='pointer'>
+                    <Link href={'#'}>Editar perfil</Link>
+                  </li>
+                  <li className='pointer'>
+                    <Link href={'#'}>Configuración</Link>
+                  </li>
+                  <li className='pointer'>
+                    <Link href={'#'}>Verificar perfil</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </nav>
+
+        <button>Inicar sesion</button>
+        <Link href={'#'}>Cerrar sesión</Link>
     </header>
   )
 }
