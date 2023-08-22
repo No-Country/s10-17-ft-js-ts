0f6299e494b33e25fb69ca06396e10b23a8d1472
Jefersonsteven <gfreycode@gmail.com>
2023-08-22 17:31:31 -0500

    (feat): syles header mobile done

--- apps/client/components/Header/Header.tsx
+++ apps/client/components/Header/Header.tsx
@@ -1,58 +1,62 @@
 'use client'
 import Link from 'next/link'
 import style from './style.module.scss'
+import { useRef } from 'react'
 
 export default function Header () {
+  const menu = useRef<HTMLDivElement>(null)
+
+  const handleMenu = () => {
+    if (menu.current) {
+      menu.current.classList.toggle(style['header__menu--active'])
+    }
+  }
+
   return (
     <header className={style.header}>
-        <span>Logo</span>
-        <nav className={style.header__navbar}>
-          <ul className={style.header__menu}>
-            <div className={style.header__navigation}>
-              <li className='pointer'>
-                <Link href={'#'}>Matches</Link>
-              </li>
-              <li className='pointer'>
-                <Link href={'#'}>Mensajes</Link>
-              </li>
-              <li className="pointer">
-                <details>
-                  <summary>Informacion</summary>
-                  <ul className={style.header__submenu}>
-                    <li className='pointer'>
-                      <Link href={'#'}>Quienes somos</Link>
-                    </li>
-                    <li className='pointer'>
-                      <Link href={'#'}>Avisos Legales</Link>
-                    </li>
-                    <li className='pointer'>
-                      <Link href={'#'}>Contacto</Link>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-            </div>
-            <li className='pointer'>
-            <details>
-                <summary>Perfil</summary>
-                <ul className={style.header__submenu}>
-                  <li className='pointer'>
-                    <Link href={'#'}>Editar perfil</Link>
-                  </li>
-                  <li className='pointer'>
-                    <Link href={'#'}>Configuración</Link>
-                  </li>
-                  <li className='pointer'>
-                    <Link href={'#'}>Verificar perfil</Link>
-                  </li>
-                </ul>
-              </details>
+      <span
+        onClick={handleMenu}
+        className={style['header__btn-menu']}
+      >🍔</span>
+      <span>Logo</span>
+      <Link href={'#'}>📩</Link>
+
+      <nav className={style.header__menu} ref={menu}>
+        <header className={style['header__menu-back']}>
+          <h3 className={style.header__username}>username</h3>
+          <span
+            onClick={handleMenu}
+          >❌</span>
+        </header>
+        <div className={style['header__menu-options']}>
+          <ul>
+            <li>
+              <Link href={'#'}>Perfil</Link>
+            </li>
+            <li>
+              <Link href={'#'}>Configuración</Link>
+            </li>
+            <li>
+              <Link href={'#'}>Ayuda</Link>
+            </li>
+            <li>
+              <Link href={'#'}>Quienes somos</Link>
+            </li>
+            <li>
+              <Link href={'#'}>Avisos Legales</Link>
             </li>
           </ul>
-        </nav>
-
-        <button>Inicar sesion</button>
+          <ul>
+            <li>
+              <Link href={'#'}>Cambiar cuenta</Link>
+            </li>
+            <li>
+              <Link href={'#'}>Salir</Link>
+            </li>
+          </ul>
+        </div>
         <Link href={'#'}>Cerrar sesión</Link>
+      </nav>
     </header>
   )
 }
