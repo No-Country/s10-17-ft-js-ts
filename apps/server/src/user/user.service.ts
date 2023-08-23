ce2e71d9a342ddb15e75b2bccebb2e1a90949389
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-08-23 14:43:40 -0300

    HU02 v1

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,9 +1,49 @@
 import { CreateUserDto, UserDto } from '@dto';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, BadRequestException } from '@nestjs/common';
+import { UserRepository, UserRepositoryKey } from './user.repository';
+import * as bcrypt from 'bcrypt';
+// import { appConfig } from '../app/app.config';
+// import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-  async create(createUserDto: CreateUserDto): Promise<UserDto> {
-    return {};
+  constructor(
+    @Inject(UserRepositoryKey)
+    private readonly userRepository: UserRepository // @Inject(appConfig.KEY) // private readonly config: ConfigType<typeof appConfig>
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<UserDto | undefined> {
+    try {
+      // hashear la pass
+      // createUserDto.password = await bcrypt.hash(
+      //   createUserDto.password,
+      //   this.config.hash.salt
+      // );
+
+      createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
+      // createUserDto.password = await bcrypt.hash(createUserDto.password, process.env.HASH_SALT);
+
+      // validar si el email ya esta usado
+      const isRegistered: boolean = await this.isRegistered(
+        createUserDto.email
+      );
+
+      if (isRegistered) {
+        throw new BadRequestException('Email already exists');
+      }
+
+      // crearlo y retornarlo.
+      return await this.userRepository.create(createUserDto);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getAll() {
+    return this.userRepository.findMany({});
+  }
+
+  async isRegistered(email: string): Promise<boolean> {
+    return this.userRepository.findByEmail(email);
   }
 }
--- libs/dto/src/lib/user/user.dto.ts
+++ libs/dto/src/lib/user/user.dto.ts
@@ -1,1 +1,72 @@
-export class UserDto{}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class UserDto {
+  @ApiProperty({
+    description: 'Name',
+    example: 'Juan',
+  })
+  firstName: string;
+
+  @ApiProperty({
+    description: 'Lastname',
+    example: 'Serrano',
+  })
+  lastName: string;
+
+  @ApiProperty({
+    description: 'Email',
+    example: 'wave@example.com ',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'profile description',
+    example: 'text of exam',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({
+    description: 'array images',
+    example: '["urlexample.jpg", "url2example.jpg"]',
+  })
+  @IsArray()
+  images: string[];
+
+  // matches: User[]
+
+  @ApiProperty({
+    description: 'birthdate',
+    example: '10/12/1998',
+  })
+  @IsNotEmpty()
+  birthdate: Date;
+
+  @ApiProperty()
+  likedBy: number[];
+
+  @ApiProperty({
+    description: 'user gender',
+    example: 'male',
+  })
+  @IsString()
+  @IsNotEmpty()
+  gender: string;
+
+  @ApiProperty({
+    description: 'user gender wants',
+    example: 'female',
+  })
+  wantsGender: string;
+
+  @ApiProperty({
+    description: 'user id',
+    example: '99a978f9-45f0-402e-a868-fabc422a3e71',
+  })
+  id: string;
+
+}
