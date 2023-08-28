ba9e6eb24ae720fa4d72acc598b1d17036bc8fc8
Jefersonsteven <gfreycode@gmail.com>
2023-08-28 06:06:39 -0500

    add: styles mobile to home

--- apps/client/app/home/page.tsx
+++ apps/client/app/home/page.tsx
@@ -1,10 +1,84 @@
 import UserMatchCard from 'components/UserMatchCard'
+import style from './page.module.scss'
+
+const user = [
+  {
+    name: 'User name',
+    photo: 'https://avatars.githubusercontent.com/u/1182328?v=5',
+    location: 'User location',
+    interests: ['Interest 1', 'Interest 2', 'Interest 3'],
+    pins: [
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg'
+    ],
+    about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem.'
+  },
+  {
+    name: 'User name',
+    photo: 'https://avatars.githubusercontent.com/u/1122328?v=5',
+    location: 'User location',
+    interests: ['Interest 1', 'Interest 2', 'Interest 3', 'Interest 4'],
+    pins: [
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg'
+    ],
+    about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem.'
+  },
+  {
+    name: 'User name',
+    photo: 'https://avatars.githubusercontent.com/u/1982328?v=5',
+    location: 'User location',
+    interests: ['Interest 1', 'Interest 2', 'Interest 3', 'Interest 4', 'Interest 5'],
+    pins: [
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://www.mundodeportivo.com/alfabeta/hero/2020/09/one-punch-man-portada.jpg?width=768&aspect_ratio=16:9&format=nowebp',
+      'https://i.blogs.es/ebfd34/naruto-nuevos-episodios-estreno-septiembre-2023/840_560.jpeg',
+      'https://es.web.img3.acsta.net/pictures/210/154/21015404_20130626115836716.jpg'
+    ],
+    about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem. Quisquam, voluptatem.'
+  }
+]
 
 export default async function Index () {
   return (
-    <div>
-      <h1>Home</h1>
-      <UserMatchCard />
-    </div>
+    <main className={style.home}>
+      {
+        user.map((user, index) => (
+          <UserMatchCard key={index} user={user} />
+        ))
+      }
+    </main>
   )
 }
--- apps/client/components/UserMatchCard/index.tsx
+++ apps/client/components/UserMatchCard/index.tsx
@@ -1,16 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
-'use cliet'
+'use client'
 import style from './style.module.scss'
 
-export default function UserMatchCard () {
+// TODO: tipar user
+interface User {
+  name: string
+  photo: string
+  location: string
+  interests: string[]
+  pins: string[]
+  about: string
+}
+
+export default function UserMatchCard ({ user }: { user: User }) {
+  function handleLike () {
+    console.log('Like')
+  }
+
+  function handleDislike () {
+    console.log('Dislike')
+  }
+
   return (
     <div className={style.user}>
-      <div className={style.user__info}>
-        <img className={style.user__photo} src="https://avatars.githubusercontent.com/u/499550?v=4" alt="User photo" />
-        <div className={style.user__details}>
-          <h2 className={style.user__name}>User name</h2>
-          <p className={style.user__location}>User location</p>
+      <div className={style.user__content}>
+        <div className={style.user__info}>
+          <img className={style.user__photo} src={user.photo} alt="User photo" />
+          <div className={style.user__details}>
+            <h2 className={style.user__name}>{user.name}</h2>
+            <p className={style.user__location}>{user.location}</p>
+          </div>
         </div>
+
+        <div className={style.user__interests}>
+          <h3 className={style['user__interests-title']}>Sus intereses</h3>
+          <ul className={style['user__interests-content']}>
+            {user.interests.map((interest, index) => (
+              <li key={index} className={style.user__interest}>
+                {interest}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={style.user__pins}>
+          <h3 className={style['user__pins-title']}>Sus pines</h3>
+          <ul className={style['user__pins-content']}>
+            {user.pins.map((pin, index) => (
+              <li key={index} className={style.user__pin} style={{ background: 'linear-gradient(45deg, rgb(186, 71, 71), rgb(33, 204, 164))' }}>
+                <img className={style['user__pin-photo']} src={pin} alt="User pin" />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={style.user__about}>
+          <h3 className={style['user__about-title']}>Sobre {user.name}</h3>
+          <p className={style['user__about-content']}>
+            {user.about}
+          </p>
+        </div>
+      </div>
+
+      <div className={style.user__buttons}>
+        <button onClick={handleDislike} className={style.user__button}>✖️</button>
+        <button onClick={handleLike} className={style.user__button}>✅</button>
       </div>
     </div>
   )
