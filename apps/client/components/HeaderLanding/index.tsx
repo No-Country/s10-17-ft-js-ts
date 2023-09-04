ba2dee773e6008aca4883f59997ff57c020fedb3
Jefersonsteven <gfreycode@gmail.com>
2023-09-04 18:24:28 -0500

    feat: landing page

--- apps/client/components/HeaderLanding/index.tsx
+++ apps/client/components/HeaderLanding/index.tsx
@@ -1,9 +1,19 @@
+/* eslint-disable no-unused-vars */
 'use client'
 import Link from 'next/link'
 import styles from './style.module.scss'
-import { useRef } from 'react'
+import { Dispatch, SetStateAction, useRef } from 'react'
+import { Icons } from 'components/Icons'
 
-export default function HeaderLanding () {
+interface Props {
+  setIsLogin: Dispatch<SetStateAction<{
+    login: boolean
+    register: boolean
+  }>>
+  handleAuth: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+}
+
+export default function HeaderLanding ({ setIsLogin, handleAuth }: Props) {
   const menu = useRef<HTMLDivElement>(null)
 
   function toggleMenu () {
@@ -12,30 +22,56 @@ export default function HeaderLanding () {
 
   return (
     <header className={styles.header}>
-      <span onClick={toggleMenu}>🍔</span>
-      <span>logo</span>
+      <span onClick={toggleMenu} className={styles['header__menu-burger']}>
+        <Icons.BurgerMenu width={40} height={40}/>
+      </span>
+      <span>
+        <Icons.LogoHome width={40} height={40}/>
+      </span>
       <nav className={styles.header__menu} ref={menu}>
         <header className={styles['header__menu-header']}>
-          <span>Logo</span>
-          <button>✖️</button>
+          <span>
+            <Icons.LogoHome width={40} height={40}/>
+          </span>
+          <button onClick={toggleMenu}>
+            <Icons.Close width={40} height={40}/>
+          </button>
         </header>
-        <details className={styles['header__menu-details']}>
-          <summary className={styles['header__menu-summary']}>
-            Información
-          </summary>
-          <ul className={styles['header__menu-list']}>
-            <li className={styles['header__menu-item']}>
-              <Link href="#">Quienes somos</Link>
-            </li>
-            <li className={styles['header__menu-item']}>
-              <Link href="#">Avisos legales</Link>
-            </li>
-            <li className={styles['header__menu-item']}>
-              <Link href="#">Contacto</Link>
-            </li>
-          </ul>
-        </details>
-        <button>Iniciar Sesion</button>
+        <main className={styles['header__menu-main']}>
+          <details className={styles['header__menu-details']}>
+            <summary className={styles['header__menu-summary']}>
+              <Icons.Info width={40} height={40}/>
+              <p>Información</p>
+            </summary>
+            <ul className={styles['header__menu-list']}>
+              <li className={styles['header__menu-item']}>
+                <Link href="/aboutus">Quienes somos</Link>
+              </li>
+              <li className={styles['header__menu-item']}>
+                <Link href="legacy">Avisos legales</Link>
+              </li>
+              <li className={styles['header__menu-item']}>
+                <Link href="/contact">Contacto</Link>
+              </li>
+            </ul>
+          </details>
+          <div className={styles['header__menu-auth']}>
+            <div className={styles['header__menu-authB']}>
+              <button
+                value='login'
+                onClick={handleAuth}
+                className='btn'>Iniciar sesion</button>
+              <button
+                value='register'
+                onClick={handleAuth}
+                className='btn'>Registrate</button>
+            </div>
+            <Link
+              href="#"
+              className={styles['header__menu-authG']}
+            >iniciar sesión con google</Link>
+          </div>
+        </main>
       </nav>
     </header>
   )
